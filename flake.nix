12e015c4057eeeeb7a8b2ed06d36e05fbb309564
ljx <2306010105@qq.com>
2025-05-14 00:13:51 +0800

    Add pysocks to Python environment

--- flake.nix
+++ flake.nix
@@ -54,6 +54,7 @@
 								scikit-learn
 								antlr4-python3-runtime
 								flask
+                pysocks
                 # If nixpkgs don't have it or you want to use a different version,
                 # you can fetch it from PyPI
                 # For wheels, check out fetchPypi src: 
